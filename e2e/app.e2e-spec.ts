da3331b59b916f767d8f5abb97191c0c2efadc34
cedricclaus <cedric.claus@gmail.com>
2017-03-16 11:43:01 +0100

    e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,47 @@
 import { BooksorderPage } from './app.po';
+import {By} from "protractor";
 
 describe('booksorder App', () => {
   let page: BooksorderPage;
 
   beforeEach(() => {
     page = new BooksorderPage();
+    page.navigateTo();
   });
 
   it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getTitleText()).toEqual('Book store!');
   });
+
+  it('should display two initial row', () => {
+    expect(page.getOrderRows().count()).toBeCloseTo(2);
+    expect(page.getTextOnLastRowByClassName('order-author')).toContain("author2");
+  });
+
+
+  it('should remove the last row', () => {
+
+    let button = page.getOnLastRowByClassName(By.buttonText("Supprimer"));
+    button.click();
+    expect(page.getOrderRows().count()).toBeCloseTo(1);
+    expect(page.getTextOnLastRowByClassName('order-author')).not.toContain("author2");
+  });
+
+  it('should add a new row from manuel', () => {
+    expect(page.getOrderRows().count()).toBeCloseTo(2);
+    page.clickToggleFormCheckBox();
+    page.getFormControl('title').sendKeys("titre");
+    page.getFormControl('author').sendKeys("author");
+    page.getFormControl('price').sendKeys("10.10");
+    page.getFormControl('quantity').sendKeys("10");
+    page.getFormControl('quantity').submit();
+
+    expect(page.getOrderRows().count()).toBeCloseTo(3);
+    expect(page.getOnLastRow(By.css('.order-quantity input')).getAttribute("value")).toEqual('10')
+
+
+  });
+
+
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,37 @@
-import { browser, element, by } from 'protractor';
+import {browser, element, by, By} from 'protractor';
+import { Locator} from "protractor/built/locators";
 
 export class BooksorderPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  getTitleText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getOrderRows(){
+    return element.all(by.className('order-row'));
+  }
+
+  getTextOnLastRowByClassName(className : string){
+    return this.getOrderRows().last().element(By.className(className)).getText();
+  }
+
+  getOnLastRowByClassName(by : Locator){
+    return this.getOrderRows().last().element(by);
+  }
+
+  clickToggleFormCheckBox(){
+    element(By.css('input[type=checkbox]')).click();
+  }
+
+  getFormControl(name : string){
+    return element(By.name(name));
+  }
+
+  getOnLastRow(by : Locator){
+    return this.getOrderRows().last().element(by);
+  }
+
 }
