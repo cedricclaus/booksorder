57c4e4fe8210119816c022b05e8f2b5b2bf78793
cedricclaus <cedric.claus@gmail.com>
2017-03-16 10:50:00 +0100

    Unit test component cooperation

--- src/app/order/order-root/order-root.component.spec.ts
+++ src/app/order/order-root/order-root.component.spec.ts
@@ -1,6 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderRootComponent } from './order-root.component';
+import {FormsModule} from "@angular/forms";
+import {OrderRowsComponent} from "../order-rows/order-rows.component";
+import {OrderManualFormComponent} from "../order-manual-form/order-manual-form.component";
+import {OrderCatalogFormComponent} from "../order-catalog-form/order-catalog-form.component";
+import {CatalogService} from "../catalog.service";
+import {Observable} from "rxjs";
+import {Book} from "../book.model";
+import {OrderRow} from "../order-row.model";
+import {By} from "@angular/platform-browser";
+
+
+class MockCatalogService{
+
+  getList(): Observable<Book[]>{
+   return Observable.of([
+     {title:"aaa",author:"xxx",price : 5.50},
+     {title:"bbb",author:"yyy",price : 6.60},
+     {title:"ccc",author:"zzz",price : 7.70},
+   ] );
+ }
+
+}
+
 
 describe('OrderRootComponent', () => {
   let component: OrderRootComponent;
@@ -8,7 +31,9 @@ describe('OrderRootComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderRootComponent ]
+      imports : [FormsModule],
+      declarations: [ OrderRootComponent,OrderRowsComponent, OrderManualFormComponent, OrderCatalogFormComponent ],
+      providers : [{provide : CatalogService, useClass : MockCatalogService}]
     })
     .compileComponents();
   }));
@@ -21,5 +46,28 @@ describe('OrderRootComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should add new row from book catalog', () => {
+    component.rows = [
+      new OrderRow("a","x",1.50,2),
+    ]
+    fixture.detectChanges();
+    let form = fixture.debugElement.query(By.css('form'));
+
+    let inputQuantity = form.query(By.css('[name=quantity]'));
+    let select = form.query(By.css('[name=selected]'));
+
+    inputQuantity.triggerEventHandler('ngModelChange',3);
+    select.triggerEventHandler('ngModelChange',{title:"bbb",author:"yyy",price : 6.60});
+    form.triggerEventHandler('ngSubmit',null);
+
+    fixture.detectChanges()
+
+    let tr = fixture.debugElement.queryAll(By.css('.order-row'));
+    expect(tr.length).toEqual(2);
+
+
+
   });
 });
