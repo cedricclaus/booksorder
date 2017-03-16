bc8e5d971cda2c0b0e9649dc14756fd62b523b1a
cedricclaus <cedric.claus@gmail.com>
2017-03-16 09:52:05 +0100

    Unit test component

--- src/app/order/order-rows/order-rows.component.spec.ts
+++ src/app/order/order-rows/order-rows.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderRowsComponent } from './order-rows.component';
+import {FormsModule} from "@angular/forms";
+import {OrderRow} from "../order-row.model";
+import {By} from "@angular/platform-browser";
+import {LOCALE_ID} from "@angular/core";
 
 describe('OrderRowsComponent', () => {
   let component: OrderRowsComponent;
@@ -8,7 +12,11 @@ describe('OrderRowsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderRowsComponent ]
+      imports : [FormsModule],
+      declarations: [ OrderRowsComponent ],
+      providers : [
+        {provide : LOCALE_ID , useValue:'fr-BE'}
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +29,49 @@ describe('OrderRowsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should compute total', (done) => {
+    component.rows = [
+      new OrderRow("a","x" , 10.10,2),
+      new OrderRow("b","y" , 20.20,1),
+    ]
+    expect(component.totalHT()).toBeCloseTo(40.40,2);
+    fixture.detectChanges();
+    let tr = fixture.debugElement.queryAll(By.css('.order-row'))
+    expect(tr.length).toEqual(2);
+    expect(tr[1].query(By.css('.order-author')).nativeElement.textContent).toEqual("y");
+
+    // ngmodel is setted async --> promise
+    fixture.whenStable().then(()=>{
+      expect(tr[1].query(By.css('.order-quantity input')).nativeElement.value).toEqual("1");
+    })
+    expect(tr[1].query(By.css('.order-ht')).nativeElement.textContent).toEqual('20,20\xa0€');
+
+    tr[1].query(By.css('.order-remove'))
+    done();
+
+
+  });
+
+  fit('should compute trigger remove event', (done) => {
+    component.rows = [
+      new OrderRow("a","x" , 10.10,2),
+      new OrderRow("b","y" , 20.20,1),
+    ]
+
+    fixture.detectChanges();
+    let tr = fixture.debugElement.queryAll(By.css('.order-row'));
+    let button = tr[1].query(By.css('.order-remove button'));
+
+    component.removeEmitter.subscribe((event)=>{
+      expect(event).toEqual(component.rows[1]);
+      done();
+    })
+
+    //click on the button
+    button.triggerEventHandler('click',null);
+
+
   });
 });
